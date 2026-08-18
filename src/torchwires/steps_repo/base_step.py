c18e28224193a73a49c62a0289515b9f6ad5b882
Youssef Samy <yosefsamy019@gmail.com>
2026-08-18 16:37:59 +0300

    Fix: device fixes

--- src/torchwires/steps_repo/base_step.py
+++ src/torchwires/steps_repo/base_step.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+from typing import Any
+
+import torch
 
 from ..models_repo.models_repo import ModelsRepo
 from ..state.batch_state import BatchState
@@ -19,10 +22,21 @@ class BaseStep(
     def execute(
             self,
             models_repo: ModelsRepo,
-            state: BatchState
+            state: BatchState,
+            device: torch.device | str,
     ):
         raise NotImplementedError()
 
     @abstractmethod
     def get_uuid(self) -> str:
         raise NotImplementedError()
+
+    def _safe_move_to_device(
+            self,
+            val: Any,
+            device: torch.device | str,
+    ):
+        if isinstance(val, torch.Tensor):
+            return val.to(device)
+        else:
+            return val
